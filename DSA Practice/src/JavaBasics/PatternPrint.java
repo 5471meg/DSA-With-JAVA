c2f660f8d1637688d5e5147ca95cf6756cf19d21
5471meg <kumarim42205@gmail.com>
2026-06-12 15:30:26 +0530

    Java Basic Program

---
+++ DSA Practice/src/JavaBasics/PatternPrint.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class PatternPrint {
+    public static void main(String[] args){
+        System.out.println(" **** ");
+        System.out.println(" *** ");
+        System.out.println(" ** ");
+        System.out.println(" * ");
+    }
+}
