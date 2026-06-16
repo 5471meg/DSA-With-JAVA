e07ccd5e83554239c5dc51db06fdaa72142d62fc
5471meg <kumarim42205@gmail.com>
2026-06-16 14:06:25 +0530

    Variable Demo

---
+++ DSA Practice/src/JavaBasics/VariableDemo.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        String name="jo";
+        System.out.println(a+b);
+        System.out.println(name);
+    }
+}
