6b08feaee05bf1e3472a47190b29f58be78e28d2
5471meg <kumarim42205@gmail.com>
2026-06-16 23:00:09 +0530

    Primitive DataType

---
+++ DSA Practice/src/JavaBasics/VariableTypeDemo.java
@@ -0,0 +1,22 @@
+package JavaBasics;
+
+public class VariableTypeDemo {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float price = 10.5f;
+        System.out.println(price);
+        int number = 30;
+        System.out.println(number);
+        //long
+        //double
+        short n = 333;
+        System.out.println(n);
+
+
+    }
+}
