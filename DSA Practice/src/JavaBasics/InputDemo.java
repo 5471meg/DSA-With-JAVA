99c145a46a36b3f50f4d434815c178f5c29cf0d9
5471meg <kumarim42205@gmail.com>
2026-06-17 20:33:37 +0530

    Concept of How to Taking Input

---
+++ DSA Practice/src/JavaBasics/InputDemo.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class InputDemo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //String input=sc.next();
+        //System.out.println(input);
+        String name=sc.nextLine();
+        System.out.println(name);
+    }
+}
