7b7e4e5303c03c3c23d3b5129459cadb817d66e2
5471meg <kumarim42205@gmail.com>
2026-06-17 20:48:23 +0530

    Add on

--- DSA Practice/src/JavaBasics/InputDemo.java
+++ DSA Practice/src/JavaBasics/InputDemo.java
@@ -9,5 +9,9 @@ public class InputDemo {
         //System.out.println(input);
         String name=sc.nextLine();
         System.out.println(name);
+        int number=sc.nextInt();
+        System.out.println(number);
+        float price=sc.nextFloat();
+        System.out.println(price);
     }
 }
