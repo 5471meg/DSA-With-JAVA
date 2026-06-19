0ba7a2e4476b7713fae7c526b129c5c7762ceb13
5471meg <kumarim42205@gmail.com>
2026-06-19 20:53:24 +0530

    Concept of Type Casting

---
+++ DSA Practice/src/JavaBasics/CircleArea.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+import java.util.Scanner;
+public class CircleArea {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float radius=sc.nextFloat();
+        float area=3.14f*radius*radius;
+        System.out.printf("%.2f",area);
+
+
+    }
+}
---
+++ DSA Practice/src/JavaBasics/TypeConversionDemo.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+import java.util.Scanner;
+public class TypeConversionDemo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+      //  int number=sc.nextFloat();We can't assign float in integer
+        float number=sc.nextInt();
+        System.out.printf("%.2f",number);
+    }
+}
+5
