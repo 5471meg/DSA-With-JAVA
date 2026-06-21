60781e5851382f9339b7d802477a99200c7fec18
5471meg <kumarim42205@gmail.com>
2026-06-21 23:49:27 +0530

    Lossy Conversion From Integer to Byte

---
+++ DSA Practice/src/JavaBasics/TypePromotionInExpression.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+public class TypePromotionInExpression {
+    public static void main(String[] args) {
+        short a=5;
+        byte b=25;
+        char c='c';
+        byte bt= (byte)(a+b+c);
+        System.out.println(bt);
+    }
+}
