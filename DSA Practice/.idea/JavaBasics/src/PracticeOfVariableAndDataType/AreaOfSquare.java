d3f9d3ac654bfeb6d2387ff725dd52b60cb3d26f
5471meg <kumarim42205@gmail.com>
2026-06-23 00:09:19 +0530

    Problem of area of square

---
+++ DSA Practice/.idea/JavaBasics/src/PracticeOfVariableAndDataType/AreaOfSquare.java
@@ -0,0 +1,10 @@
+package PracticeOfVariableAndDataType;
+import java.util.Scanner;
+public class AreaOfSquare {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float s=sc.nextFloat();
+        float area=(s*s);
+        System.out.println("Area of square is:"+area);
+    }
+}
