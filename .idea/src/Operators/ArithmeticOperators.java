da32ea6dd2c8f98e2fd6eeb451468f698387b571
5471meg <kumarim42205@gmail.com>
2026-06-24 20:23:06 +0530

    Basic Problems of Java

---
+++ .idea/src/Operators/ArithmeticOperators.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int A =10;
+        int B =5;
+        System.out.println("add =" +(A+B));
+        System.out.println("difference =" +(A-B));
+    }
+}
