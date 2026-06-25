c7114816772be8f8e4e8ceace1b971026311c096
5471meg <kumarim42205@gmail.com>
2026-06-25 23:43:15 +0530

    Practice Problem of Operators

---
+++ src/OperatorsPractice/PracticeQues1.java
@@ -0,0 +1,11 @@
+package OperatorsPractice;
+
+public class PracticeQues1 {
+    public static void main(String[] args) {
+        int X = 2, Y = 5;
+        int exp1 = (X*Y/X);
+        int exp2 = (X*(Y/X));
+        System.out.println(exp1 + " ,");
+        System.out.println(exp2);
+    }
+}
