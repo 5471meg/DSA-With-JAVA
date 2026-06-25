865d11779d119115b6736950abb3a9401c524854
5471meg <kumarim42205@gmail.com>
2026-06-25 23:58:12 +0530

    Practice Problem of Operators

---
+++ src/OperatorsPractice/PracticeQues2.java
@@ -0,0 +1,16 @@
+package OperatorsPractice;
+
+public class PracticeQues2 {
+    public static void main(String[] args) {
+        int X = 200 , Y = 50 , Z = 100;
+        if (X>Y && Y>Z){
+            System.out.println("Hello");
+        }
+        if(Z>Y && Z<X){
+            System.out.println("Java");
+        }
+        if((Y+200) < X&& (Y+150) < Z){
+            System.out.println("Hello Java");
+        }
+    }
+}
