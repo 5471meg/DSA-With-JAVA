d0064ea2de20083ec81782c83d6f46c05483b39c
5471meg <kumarim42205@gmail.com>
2026-06-26 00:07:51 +0530

    Practice Problem of Assignment Operators

---
+++ src/OperatorsPractice/PracticeQues3.java
@@ -0,0 +1,12 @@
+package OperatorsPractice;
+
+public class PracticeQues3 {
+    public static void main(String[] args) {
+        int X , Y , Z;
+        X=Y=Z=2;
+        X+=Y;
+        Y-=Z;
+        Z/=(X+Y);
+        System.out.println(X+ " " + Y + " "+Z);
+    }
+}
