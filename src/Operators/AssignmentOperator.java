d898337fc4368fdb52ee2acfd534976456223a5a
5471meg <kumarim42205@gmail.com>
2026-06-25 22:31:03 +0530

    Basics of Assignment Operator

---
+++ src/Operators/AssignmentOperator.java
@@ -0,0 +1,27 @@
+package Operators;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int A=10;
+      //  A=A+10;
+        A+=10;
+        int B=20;
+        //B=B-20
+        B-=20;
+        int C=30;
+        //C=C*30;
+        C*=30;
+        int D=40;
+        //D=D/40;
+        D/=40;
+        int E=50;
+        //E=E%50
+        E%=50;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+
+    }
+}
