3fc6311958281d20ddf70da53647235faa6c7270
5471meg <kumarim42205@gmail.com>
2026-06-26 11:41:42 +0530

    Print the Largest of two

---
+++ src/ConditionalStatements/LargestOfTwo.java
@@ -0,0 +1,14 @@
+package ConditionalStatements;
+
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int A=1;
+        int B=6;
+        if(A>=B){
+            System.out.println(" A is largest of 2 ");
+        }
+        else{
+            System.out.println(" B is  largest of 2 ");
+        }
+    }
+}
