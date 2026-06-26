bfa92e1657bc581e6d1bab080e7ccd4173aad0fc
5471meg <kumarim42205@gmail.com>
2026-06-26 11:59:14 +0530

    Finding Even Odd using if else Statement

---
+++ src/ConditionalStatements/EvenOrOddNumber.java
@@ -0,0 +1,15 @@
+package ConditionalStatements;
+import java.util.Scanner;
+public class EvenOrOddNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+
+        if(number%2==0){
+            System.out.println(" Number is Even ");
+        }
+        else{
+            System.out.println(" Number is Odd ");
+        }
+    }
+}
