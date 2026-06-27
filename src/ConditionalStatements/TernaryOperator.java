0e2333dddf2805bbaecf0b2be87ef0219872e73a
5471meg <kumarim42205@gmail.com>
2026-06-27 21:28:03 +0530

    Finding the number is even or odd using ternary operator

---
+++ src/ConditionalStatements/TernaryOperator.java
@@ -0,0 +1,10 @@
+package ConditionalStatements;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int number = 4;
+        //ternary Operator
+        String type = ((number%2) == 0) ? "even" : "odd";
+        System.out.println(type);
+    }
+}
