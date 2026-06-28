a64168b1e083c140283a205f16ecbd8264d8ec6e
5471meg <kumarim42205@gmail.com>
2026-06-28 23:06:55 +0530

    Finding Positive Negative Number using If Else Statement

--- src/PracticeOfConditionalSatement/PositiveNegative.java
+++ src/PracticeOfConditionalSatement/PositiveNegative.java
@@ -6,7 +6,7 @@ public class PositiveNegative {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        if (number >= 0) {
+        if (number > 0) {
             System.out.println("The number is positive");
         }
         else {
