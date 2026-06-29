0636a228c8e70dcc1c306f4b286c5056c68f0a06
5471meg <kumarim42205@gmail.com>
2026-06-29 23:56:54 +0530

    Printing Using Basics of For Loop

---
+++ src/Loops/ForLoopBasics.java
@@ -0,0 +1,11 @@
+package Loops;
+
+
+public class ForLoopBasics {
+    public static void main(String[] args) {
+        //int i = 1;
+        for(int i = 1; i <= 20; i++){
+            System.out.println(" Hello World ");
+        }
+    }
+}
